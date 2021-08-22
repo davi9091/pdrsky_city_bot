3d5086889d1ffbb3d9cd2818b55116accbe8bd49
Arkady Rubtsov <davi9091@MacBook-Air-Arkady.local>
2021-08-22 18:31:26 +0300

    initial commit

---
+++ index.ts
@@ -0,0 +1,34 @@
+import {Telegraf} from 'telegraf';
+import config from './config.json';
+
+const run = () => {
+  const bot = new Telegraf(config.token);
+
+  const regexCityPattern = new RegExp(/питерск[\wа-я]*/gi);
+  const regexCutPattern = new RegExp(/питерск/gi);
+
+  bot.command('quit', (ctx) => {
+    ctx.telegram.leaveChat(ctx.message.chat.id);
+    ctx.leaveChat();
+  })
+
+  bot.start((ctx) => ctx.reply('Отличный город у вас тут'));
+
+  bot.on('text', (ctx) => {
+    console.debug(ctx.chat.id, ctx.chat.type);
+    const textMatch = ctx.message.text.match(regexCityPattern);
+    if (textMatch && textMatch.length) {
+      const text = textMatch.toString();
+      const wordEnd = text.replace(regexCutPattern, '');
+      ctx.telegram.sendMessage(ctx.message.chat.id, `Пидорск${wordEnd}*`);
+    }
+  })
+
+  bot.launch();
+  console.debug('Started bot');
+
+  process.once('SIGINT', () => bot.stop('SIGINT'));
+  process.once('SIGTERM', () => bot.stop('SIGTERM'));
+}
+
+run();
