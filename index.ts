57637d9fa7e039f10573c3d4dd839146feb47dab
Arkady Rubtsov <davi9091@MacBook-Air-Arkady.local>
2021-09-04 22:42:34 +0300

    added more matching patterns

--- index.ts
+++ index.ts
@@ -1,11 +1,47 @@
 import {Telegraf} from 'telegraf';
 import config from './config.json';
 
+type Pattern = {
+  cut: RegExp;
+  word: RegExp;
+  replace: (ending?: string) => string;
+}
+
+const PATTERNS: Record<string, Pattern> = {
+  CITY: {
+    cut: new RegExp(/питерск/gi),
+    word: new RegExp(/питерск[\wа-я]*/gi),
+    replace: (ending = 'кий') => `Пидорск${ending}`,
+  },
+  PEOPLE_SINGULAR: {
+    cut: new RegExp(/питере/gi),
+    word: new RegExp(/питере[\wа-я]*/gi),
+    replace: () => `Пидор`,
+  },
+  PEOPLE_PLURAL: {
+    cut: new RegExp(/питерц/gi),
+    word: new RegExp(/питерц[\wа-я]*/gi),
+    replace: () => `Пидоры`,
+  }
+}
+
+function textHandler (text: string, pattern: Pattern): string | undefined {
+  const textMatch = text.match(pattern.word);
+
+  if (textMatch && textMatch.length) {
+    const resultStrings = textMatch.map(match => {
+        const text = match.toString();
+        const wordEnd = text.replace(pattern.cut, '');
+        return pattern.replace(wordEnd);
+    });
+    
+    return resultStrings.join(', ');
+  }
+}
+
 const run = () => {
   const bot = new Telegraf(config.token);
 
-  const regexCityPattern = new RegExp(/питерск[\wа-я]*/gi);
-  const regexCutPattern = new RegExp(/питерск/gi);
 
   bot.command('quit', (ctx) => {
     ctx.telegram.leaveChat(ctx.message.chat.id);
@@ -15,18 +51,13 @@ const run = () => {
   bot.start((ctx) => ctx.reply('Отличный город у вас тут'));
 
   bot.on('text', (ctx) => {
-    console.debug(ctx.chat.id, ctx.chat.type);
-    const textMatch = ctx.message.text.match(regexCityPattern);
-    if (textMatch && textMatch.length) {
-      const resultStrings = textMatch.map(match => {
-        const text = match.toString();
-        const wordEnd = text.replace(regexCutPattern, '');
-        return `Пидорск${wordEnd}*`;
-      })
-
-      ctx.telegram.sendMessage(ctx.message.chat.id, resultStrings.join(', '));
-    }
-  })
+    Object.values(PATTERNS).forEach((pattern) => {
+      const messageText = textHandler(ctx.message.text, pattern);
+      if (messageText) {
+        ctx.telegram.sendMessage(ctx.message.chat.id, messageText);
+      }
+    })
+  });
 
   bot.launch();
   console.debug('Started bot');
