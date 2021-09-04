71c899182b827555489aa295f50dc7472c661633
Arkady Rubtsov <davi9091@MacBook-Air-Arkady.local>
2021-09-04 23:09:00 +0300

    fixed whitespace bug

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import {Telegraf} from 'telegraf';
 import config from './config.json';
 
-const complimentRegexp = new RegExp(/^(молодец|спасибо|хороший\wбот|thanks|good\wbot)/gi);
+const complimentRegexp = new RegExp(/^(молодец|спасибо|хороший\sбот|thanks|good\sbot)/gi);
 const complimentStickerId = 'CAACAgIAAxkBAAIBkGEzz2hwgW9UhrLXLUVaJZ9BK2nrAAIOAAM3-_0oxYFS1bnA5nogBA';
 
 type Pattern = {
