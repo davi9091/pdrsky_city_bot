e9f63c4c595fe9e7765fa3016292b1ca11cb0b87
Arkady Rubtsov <davi9091@MacBook-Air-Arkady.local>
2021-09-05 01:12:15 +0300

    added readme, added asterisks

---
+++ README.md
@@ -0,0 +1,21 @@
+
+#Pdrksy bot
+
+This might look homophobic, but it's really not
+
+##Install
+
+
+###Create a config.json file
+```
+{
+  "token": "your_token",
+  "botId": "your_bot_id"
+}
+```
+
+###You already know this one
+```
+npm i
+npm run start
+```
--- index.ts
+++ index.ts
@@ -14,17 +14,17 @@ const PATTERNS: Record<string, Pattern> = {
   CITY: {
     cut: new RegExp(/питерск/gi),
     word: new RegExp(/питерск[\wа-я]*/gi),
-    replace: (ending = 'кий') => `Пидорск${ending}`,
+    replace: (ending = 'кий') => `Пидорск${ending}*`,
   },
   PEOPLE_SINGULAR: {
-    cut: new RegExp(/питере/gi),
-    word: new RegExp(/питере[\wа-я]*/gi),
-    replace: () => `Пидор`,
+    cut: new RegExp(/питерец/gi),
+    word: new RegExp(/питерец/gi),
+    replace: () => 'Пидор*',
   },
   PEOPLE_PLURAL: {
     cut: new RegExp(/питерц/gi),
     word: new RegExp(/питерц[\wа-я]*/gi),
-    replace: () => `Пидоры`,
+    replace: (ending = 'ы') => `Пидор${ending}*`,
   },
 }
 
